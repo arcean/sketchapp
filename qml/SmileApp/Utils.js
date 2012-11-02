d992d50878489447096767f493c9336e454b4d2b
Tomasz Pieniążek <t.pieniazek@gazeta.pl>
2012-11-02 20:22:40 +0100

    Added missing files.
    Signed-off-by: Tomasz Pieniążek <t.pieniazek@gazeta.pl>

---
+++ qml/SmileApp/Utils.js
@@ -0,0 +1,19 @@
+function createObject(sourceFile, parentObject) {
+    var component = Qt.createComponent(sourceFile);
+    var guiObject = component.createObject(parentObject);
+
+    if (guiObject == null) {
+        console.log("Error creating object");
+    }
+    else {
+        return guiObject
+    }
+}
+
+function cloneObject(objectToClone) {
+    var clone = {};
+    for (var attribute in objectToClone) {
+        clone[attribute] = objectToClone[attribute];
+    }
+    return clone;
+}
